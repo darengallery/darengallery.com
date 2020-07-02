89c2a0e0f60be33bf8ac76c2d4ea60a908d60b17
Daren Gallery <darengallery@gmail.com>
2020-07-02 22:35:03 +0100

    added code

--- PHP/sendmail.php
+++ PHP/sendmail.php
@@ -1,23 +1,19 @@
 <?php
 $name = $_POST['name'];
-$playersName = $_POST['playersName'];
-$playersSchool = $_POST['playersSchool'];
-$dob = $_POST['dob'];
-$gender = $_POST['gender'];
-$address = $_POST['address'];
-$postcode = $_POST['postcode'];
-$homePhone = $_POST['homePhone'];
-$contactPhone = $_POST['contactPhone'];
 $email = $_POST['email'];
-$medical = $_POST['medical'];
-$message = $_POST['message'];
-$optradio = $_POST['optradio'];
+$opt-1 = $_POST['development'];
+$opt-2= $_POST['maintenance'];
+$opt-3 = $_POST['amendments'];
+$opt-4 = $_POST['hosting'];
+$opt-5 = $_POST['images'];
+$opt-6 = $_POST['media'];
 
-$formcontent=" From: $name \n Players Name: $playersName \n Players School:  $playersSchool \n $gender gender: \n Address: $address \n postcode: $postcode \n Home Phone: $homePhone \n Contact Phone: $contactPhone \n Medical: $medical \n Message: $message \n First Aid: $optradio";
-$recipient = "admin@essexrangers2020.org";
+
+$formcontent=" From: $name \n Senders Name: $email \n Email:  $opt-1 \n Web Development $opt-2 \n Web Maintenance $opt-3 \n Web Amendments $opt-4 \n Hosting and Servers $opt-5 \n Images and Media $opt-6 \n Printed Media";
+$recipient = "darengallery@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-header('Location: http://www.essexrangers2020.org/thank_you.html');
+header('Location: http://www.darengallery.com/thank_you.html');
 exit;
 ?>
