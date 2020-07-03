ae226bc947c9306a46b34d9d8ed64f20daaa1364
Daren Gallery <darengallery@gmail.com>
2020-07-03 20:46:47 +0100

    added code

--- PHP/sendmail.php
+++ PHP/sendmail.php
@@ -1,12 +1,12 @@
 <?php
 $name = $_POST['name'];
 $email = $_POST['email'];
-isset($_POST['form_development']);
-isset($_POST['form_maintenance']);
-isset($_POST['form_amendments']);
-isset($_POST['form_hosting']);
-isset($_POST['form_images']);
-isset($_POST['form_media']);
+if (isset($_POST['form_development']));
+if (isset($_POST['form_maintenance']));
+if (isset($_POST['form_amendments']));
+if (isset($_POST['form_hosting']));
+if (isset($_POST['form_images']));
+if (isset($_POST['form_media']));
 $formcontent=" From: $name \n Email: $email \n Maintenance: $form_maintenance \n Development: $form_development \n Amendments: $form_amendments \n Hosting: $form_hosting \n Images: $form_images \n $form_media";
 $recipient = "daren_gallery@hotmail.com";
 $subject = "Contact Form";
