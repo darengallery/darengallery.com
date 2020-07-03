2af08d5451404941f6cad41adc6e8bb6412588f2
Daren Gallery <darengallery@gmail.com>
2020-07-03 20:30:45 +0100

    added code

--- PHP/sendmail.php
+++ PHP/sendmail.php
@@ -1,40 +1,16 @@
 <?php
-
-if (isset($_post['name'])) {
-    $name = $_POST['name'];
-}
-if (isset($_post['email'])) {
-    $email = $_POST['email'];
-}
-if (isset($_POST['development'])) {
-    $development = $_POST['development'];
-}
-
-if (isset($_POST['maintenace'])) {
-    $maintenace = $_POST['maintenace'];
-}
-
-if (isset($_POST['amendments'])) {
-    $amendments = $_POST['amendments'];
-}
-if (isset($_POST['hosting'])) {
-    $hosting = $_POST['hosting'];
-}
-
-if (isset($_POST['images'])) {
-    $images = $_POST['images'];
-}
-
-if (isset($_POST['media'])) {
-    $media = $_POST['media'];
-}
-$formcontent=" From: $name \n Email: $email \n Web Maintenance: $maintenance \n Web Amendments: $amendments \n Hosting and Server: $hosting \n Images: $images \n Printed Media: $media";
-$recipient = "darengallery@gmail.com";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$form_development = $_POST['form_development'];
+$form_maintenance = $_POST['form_maintenance'];
+$form_amendments = $_POST['form_amendments'];
+$form_hosting = $_POST['form_hosting'];
+$form_images = $_POST['form_images'];
+$form_media = $_POST['form_media'];
+$formcontent=" From: $name \n Email: $email \n Maintenance: $form_maintenance \n Development: $form_development \n Amendments: $form_amendments \n Hosting: $form_hosting \n Images: $form_images \n $form_media";
+$recipient = "daren_gallery@hotmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
-?>
-
-
 ?>
