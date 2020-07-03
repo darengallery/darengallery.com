7a0749a4ba2e351c33f18f00465400d30feb0607
Daren Gallery <darengallery@gmail.com>
2020-07-03 15:43:14 +0100

    added code

--- PHP/sendmail.php
+++ PHP/sendmail.php
@@ -1,16 +1,34 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$development = $_POST['development'];
-$maintenance = $_POST['maintenance'];
-$amendments = $_POST['amendments'];
-$hosting = $_POST['hosting'];
-$images = $_POST['images'];
-$media = $_POST['media'];
+
+if (isset($_POST['development'])) {
+    $development = $_POST['development'];
+}
+
+if (isset($_POST['maintenace'])) {
+    $maintenace = $_POST['maintenace'];
+}
+
+if (isset($_POST['amendments'])) {
+    $amendments = $_POST['amendments'];
+}
+if (isset($_POST['hosting'])) {
+    $hosting = $_POST['hosting'];
+}
+
+if (isset($_POST['images'])) {
+    $images = $_POST['images'];
+}
+
+if (isset($_POST['media'])) {
+    $media = $_POST['media'];
+}
 $formcontent=" From: $name \n Phone: $phone \n maintenance Back: $maintenance \n amendments: $amendments \n hosting: $hosting \n images: $images \n media: $message";
 $recipient = "darengallery@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
+
+
 ?>
