562a145ddb450ca1c62d8763277c113618e91969
Daren Gallery <darengallery@gmail.com>
2021-01-05 23:43:41 +0000

    code

--- php/contact-form.php
+++ php/contact-form.php
@@ -2,7 +2,7 @@
 session_cache_limiter('nocache');
 header('Expires: ' . gmdate('r', 0));
 header('Content-type: application/json');
-$to = 'daren_gallery@hotmail.com';
+$to = 'darengallery@gmail.com';
 $subject = $_POST['subject'];
 if(isset($_POST['email'])) {
 	$name = $_POST['name'];
